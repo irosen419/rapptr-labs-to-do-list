b6cdfdac5e72c1a7a9ab5cfd32270c77c06fa860
irosen419 <irosen419@gmail.com>
2020-12-30 22:08:32 -0500

    login button is disabled until both email and password are valid.

--- src/Components/LoginFrom.js
+++ src/Components/LoginFrom.js
@@ -5,13 +5,18 @@ function LoginForm() {
     const [loginInfo, setLoginInfo] = useState({ email: "", password: "" })
     const [invalidEmail, setInvalidEmail] = useState(false)
     const [invalidPassword, setInvalidPassword] = useState(false)
+    const [login, setLogin] = useState(true)
+
+    useEffect(() => {
+        setLogin(!(!invalidEmail && !invalidPassword && !!loginInfo.email && !!loginInfo.password))
+    }, [invalidEmail, invalidPassword])
 
     const changeHandler = ({ target: { name, value } }) => {
-        validateUser(name, value)
+        validateUserInfo(name, value)
         setLoginInfo({ ...loginInfo, [name]: value })
     }
 
-    const validateUser = (name, value) => {
+    const validateUserInfo = (name, value) => {
         if (name === 'email') {
             if (!value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
                 setInvalidEmail(true)
@@ -30,9 +35,26 @@ function LoginForm() {
     return (
         <form>
             <label htmlFor="email">Email</label>
-            <input className={invalidEmail ? 'invalid' : null} name="email" placeholder="​user@rapptrlabs.com​" type="text" maxLength="50" value={loginInfo.email} onChange={changeHandler} />
+
+            <input className={invalidEmail ? 'invalid' : null}
+                name="email"
+                placeholder="​user@rapptrlabs.com​"
+                type="text"
+                maxLength="50"
+                value={loginInfo.email}
+                onChange={changeHandler} />
+
             <label htmlFor="password">Password</label>
-            <input className={invalidPassword ? 'invalid' : null} name="password" placeholder="Must be between 4 and 16 characters" type="password" maxLength="16" value={loginInfo.password} onChange={changeHandler} />
+
+            <input className={invalidPassword ? 'invalid' : null}
+                name="password"
+                placeholder="Must be between 4 and 16 characters"
+                type="password"
+                maxLength="16"
+                value={loginInfo.password}
+                onChange={changeHandler} />
+
+            <input type="submit" value="Login" disabled={login} />
         </form>
     )
 }
